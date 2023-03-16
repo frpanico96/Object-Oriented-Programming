b4282e7f5521cd4d6cac96504e2d578644653c91
francescopanico <frpanico96@gmail.com>
2023-03-16 22:36:14 +0100

    [OOP] Java Learning - Resume

--- Chapter_2/DogDoorSimulator.java
+++ Chapter_2/DogDoorSimulator.java
@@ -8,10 +8,23 @@ public class DogDoorSimulator {
     {
         DogDoor testDoor = new DogDoor();
         Remote testRemote = new Remote(testDoor);
+
+        System.out.println("Fido scratches at the door");
         System.out.println("Fido barks to go outside...");
         testRemote.pressButton();
         System.out.println("\nFido has gone outside...");
         System.out.println("\nFido's all done...");
+        try
+        {
+            Thread.currentThread();
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e) {}
+        System.out.println("Fido starts barking...");
+        System.out.println("...but he's stuck outside");
+        System.out.println("Fido scratches at the door.");
+        System.out.println("so Gina grabs the remote control");
+        testRemote.pressButton();
         System.out.println("\nFido's back inside...");
     }
 
